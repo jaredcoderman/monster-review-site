925f9aa00e0586568444f5b2507a830bf5f68366
Jared Head <jaredhead@jareds-mbp.myfiosgateway.com>
2021-11-10 17:28:08 -0500

    Added error handling

--- app/controllers/api/v1/monsters_controller.rb
+++ app/controllers/api/v1/monsters_controller.rb
@@ -5,6 +5,19 @@ class Api::V1::MonstersController < ApiController
   end
 
   def create
-    binding.pry
+    @monster = Monster.new(monster_params)
+    
+    if @monster.save
+      render json: { response: "Monster added successfully" }
+    else
+      errors = @monster.errors.full_messages.to_sentence
+      render json: { response: errors }
+    end
+  end
+
+  private
+
+  def monster_params
+    params.require(:monster).permit(:name, :description, :classification, :habitat)
   end
 end
