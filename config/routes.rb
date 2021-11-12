29790673e22c32f27b1fe5b8247128ae3dbd5ce9
ghpelton1979 <ghpelton@gmail.com>
2021-11-12 15:44:37 -0500

    Merge issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "homes#index"
 
-  get '/users/sign_out', to: "sessions#new"
-
   namespace :api do
     namespace :v1 do
       resources :monsters, only: [:index]
