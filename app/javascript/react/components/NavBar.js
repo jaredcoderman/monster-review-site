743bbc49d4300841b62bfefec143f28267f7d845
ghpelton1979 <ghpelton@gmail.com>
2021-11-15 16:44:16 -0500

    end of day

--- app/javascript/react/components/NavBar.js
+++ app/javascript/react/components/NavBar.js
@@ -8,11 +8,15 @@ import MonsterShow from "./MonsterShow"
 const NavBar = props => {
   return(
     <div>
-      <div className ="navbar"> 
-      <Link to="/monsters/new">Add a Monster </Link>
-      <Link to="/"> Monsters </Link>
-      <a href ="/users/sign_in"> Sign In </a> 
-      <a href ="/users/sign_up"> Sign Up </a>
+      <div className ="top-bar"> 
+        <div className="top-bar-left">
+          <Link to="/monsters/new">Add a Monster </Link>
+        </div>
+          <Link to="/"> Monsters </Link>
+        <div className="top-bar-right">
+          <a href ="/users/sign_in"> Sign In </a>/<a href ="/users/sign_up"> Sign Up </a>
+          <a href ="/users/sign_out" data-method="delete"> Sign Out</a>
+        </div>  
       </div>
 
       <div>
